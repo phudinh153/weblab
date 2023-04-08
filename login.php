e11bcb24274c1851867ee19531bea4ca9d4fe99c
phudinh153 <phuxa153@gmail.com>
2023-04-08 17:20:20 +0700

    validate login

--- login.php
+++ login.php
@@ -3,7 +3,7 @@
     
     <div class="col-6 offset-3" style="border-radius: 10px; background-color: yellow; font-weight: 570; font-size: 20px">
     <h1 class="text-center">Login</h1>
-        <form action="login_processing.php" method="post" >
+        <form id="login" action="login_processing.php" method="post" onsubmit="validateForm">
             <div class="mb-3">
             <label for="email" class="form-label">Email</label>
             <input type="text" class="form-control" name="email" id="email" value="<?php echo isset($_SESSION['email']) ? $_SESSION['email'] : ''; ?>" required>
@@ -17,7 +17,7 @@
             
             
             <div class="mb-3">
-                <button class="btn btn-success">Login</button>
+                <button type="submit" class="btn btn-success">Login</button>
             </div>
             
         
--- register.php
+++ register.php
@@ -16,13 +16,15 @@
             
             <div class="mb-3">
             <label for="password" class="form-label">Password</label>
-            <input type="password" class="form-control" name="password" id="password" required>
+            <input type="password" class="form-control" name="password" id="password" oninput="checkPass()" required>
             </div>
+            <div class="alert alert-danger" id="password-message" style="display: none;">Password must have 8 letters and contain at least one lowercase letter, one uppercase letter, one number, and one special character.</div>
            
             <div class="mb-3">
             <label for="confirm_password" class="form-label">Confirm Password</label>
-            <input type="password" class="form-control" name="confirm_password" id="confirm_password" required>
+            <input type="password" class="form-control" name="confirm_password" id="confirm_password" oninput="conPass()" required>
             </div>
+            <div class="alert alert-danger" id="conf_password-message" style="display: none;">Confirm Password is not the same as Password</div>
             
             
             <div class="mb-3">
@@ -34,4 +36,61 @@
            
     </div>
   </div>
+<script>
+  function checkPass(){
+  const passw = document.querySelector("#password");
+  const password = document.getElementById("password").value;
+  const passwordMessage = document.getElementById("password-message");
+  const conf_passw = document.querySelector("#confirm_password");
+  if (password.length === 0) {
+    passwordMessage.style.display = "none";
+  } else {
+    passwordMessage.style.display = "block";
+  }
+  passw.addEventListener('input',(e)=>{
+    if (!validateUsername(e.target.value)) {
+      passw.classList.add("border-danger");
+      passw.classList.add("border-3");
+      conf_passw.disabled = true;
+    }else{
+      conf_passw.disabled = false;
+      passwordMessage.style.display = "none";
+      passwordMessage.classList.remove("alert");
+      passwordMessage.classList.remove("alert-danger");
+    }
+    
+    
+  });
+}
+  function conPass(){
+  const password = document.getElementById("password").value;
+  const conf_passw = document.querySelector("#confirm_password")
+  const confpasswordMess = document.getElementById("conf_password-message");
+  const confirmPassword = document.getElementById("confirm_password").value;
+  console.log(conf_passw.value.length);
+  if (conf_passw.value.length === 0) {
+    confpasswordMess.style.display = "none";
+  } else {
+    confpasswordMess.style.display = "block";
+  }
+    conf_passw.addEventListener('input',(e)=>{
+    if (e.target.value !== password ) {
+      conf_passw.classList.add("border-danger");
+      conf_passw.classList.add("border-3");
+    }
+    else{
+      confpasswordMess.style.display = "none";
+      conf_passw.classList.remove("border-danger");
+      conf_passw.classList.remove("border-3");
+    }
+  });
+  }
+  
 
+  
+  function validateUsername(password) {
+  const regex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[.@$!%*?&])[A-Za-z\d.@$!%*?&]{8,}$/;
+  return regex.test(password);
+  }
+
+</script>
