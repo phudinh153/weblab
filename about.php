b84ea9e1cb507302d032eccf26fa85799ca7506a
phudinh153 <phuxa153@gmail.com>
2023-05-03 22:10:35 +0700

    Update about.php

--- about.php
+++ about.php
@@ -1,74 +1,90 @@
-<style>
-    section{
-        margin-bottom: 300px;
-        display: flex;
-        flex-direction: row;
-        justify-content: center;
-        align-items: center;
-        flex-wrap: wrap;
-        margin-top: 50px;
-    }
-    .location{
-        color: white;
-    }
-    #map {
-        height: 400px;
-        width: 400px;
-        overflow: hidden;
-        
+<!DOCTYPE html>
+<!--
+ @license
+ Copyright 2019 Google LLC. All Rights Reserved.
+ SPDX-License-Identifier: Apache-2.0
+-->
+<html>
+  <head>
+    <title>Add Map</title>
+    <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
+
+    <link rel="stylesheet" type="text/css" href="./style.css" />
+    <script type="module" src="./index.js"></script>
+    <style>
+      /* Set the size of the div element that contains the map */
+      #map {
+          height: 400px; /* The height is 400 pixels */
+          width: 100%; /* The width is the width of the web page */
       }
-</style>
-<section >
-    <div class="location">
-        <h1>Location:</h1> 
-    </div>
-
+    </style>
+  </head>
+  <body>
+    <h3>Location:</h3>
+    <!--The div element for the map -->
     <div id="map"></div>
-    <script src="https://cdn.jsdelivr.net/npm/leaflet@1.7.1/dist/leaflet.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+
+    <!-- prettier-ignore -->
+    <script>(g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})
+        ({key: "AIzaSyCCFdkhCb0qzhWpLwv3FR8Vd8259ScHGIU", v: "beta"});</script>
     <script>
-         // The latitude and longitude coordinates
-       var lat = 10.77331;
-       var lon = 106.65968;
+      // Initialize and add the map
+let map;
 
-       // Create the map centered on the specified location
-      var map = L.map('map').setView([lat, lon], 15);
+async function initMap() {
+  const address = "1600 Amphitheatre Parkway, Mountain View, CA";
+  // const geocoder = new google.maps.Geocoder();
+  // geocoder.geocode({ address: address }, (results, status) => {
+  //   if (status === "OK") {
+  //     // Get the coordinates for the first result
+  //     const position = {
+  //       lat: results[0].geometry.location.lat(),
+  //       lng: results[0].geometry.location.lng(),
+  //     };
 
-        // Add the OpenStreetMap tile layer
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
-        }).addTo(map);
+  //     // Request needed libraries.
+  //     //@ts-ignore
+  //     const { Map } = await google.maps.importLibrary("maps");
+  //     const { AdvancedMarkerView } = await google.maps.importLibrary("marker");
 
-      //var address = "1600 Amphitheatre Parkway, Mountain View, CA 94043";
-      
-      // Geocode the address to get the latitude and longitude coordinates
-    //   axios.get("https://nominatim.openstreetmap.org/search?q=" + address + "&format=json")
-    //     .then(function (response) {
-    //       var lat = response.data[0].lat;
-    //       var lon = response.data[0].lon;
+  //     // The map, centered at the location of the address
+  //     map = new Map(document.getElementById("map"), {
+  //       zoom: 15,
+  //       center: position,
+  //       mapId: "DEMO_MAP_ID",
+  //     });
 
-    //       // Create the map centered on the geocoded location
-    //       var map = L.map('map').setView([lat, lon], 15);
+  //     // The marker, positioned at the location of the address
+  //     const marker = new AdvancedMarkerView({
+  //       map: map,
+  //       position: position,
+  //       title: address,
+  //     });
+  //   }
+  //   });
+  // The location of 10.772075,106.6553215
+  const position = { lat: 10.772075, lng: 106.6553215 };
+  // Request needed libraries.
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerView } = await google.maps.importLibrary("marker");
 
-    //       // Add the OpenStreetMap tile layer
-    //       L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    //         attribution: '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
-    //       }).addTo(map);
+  // The map, centered at bachkhoa
+  map = new Map(document.getElementById("map"), {
+    zoom: 16,
+    center: position,
+    mapId: "DEMO_MAP_ID",
+  });
 
-    //       // Add a marker to the geocoded location
-    //       var marker = L.marker([lat, lon]).addTo(map);
-    //     })
-    //     .catch(function (error) {
-    //       console.log(error);
-    //     });
+  // The marker, positioned at bachkhoa
+  const marker = new AdvancedMarkerView({
+    map: map,
+    position: position,
+    title: 'Back Khoa',
+  });
+}
+
+initMap();
     </script>
-   
-</section>
-
-
-
-
-
-
-
-
+  </body>
+</html>
